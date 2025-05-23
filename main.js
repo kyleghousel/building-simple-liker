13bdb2ba00b7fcd12a24f8ed82a1da47306ba572
Kyle Housel <kylegranthousel@gmail.com>
2025-05-23 09:33:25 -0400

    Select glyphs, add test event listener with cb fn

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeGlyphs = document.querySelectorAll('.like-glyph')
+
+const toggleLike = () => {
+  console.log('yay')
+}
+
+likeGlyphs.forEach(glyph => glyph.addEventListener('click', toggleLike))
+
 
 
 
